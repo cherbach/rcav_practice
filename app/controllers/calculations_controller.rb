21ba158424e6c3fd3a2548f22cd6a5f9692f52c7
cherbach <christinaherbach@dhcp-10-101-62-17.wireless.northwestern.private>
2015-05-11 15:32:11 -0500

    rcav_practice Complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,25 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+  	number = params[:id].to_i
+    @square = number**2
+  end
+
   def sqrt
+  	number = params[:id].to_i
+    @sqrt = Math.sqrt(number)
+  end
+
+  def loan_payment
+  	@rate = params[:rate].to_f
+  	@number = params[:number].to_f
+  	@principal = params[:principal].to_f   
+
+    @loan_payment = (@rate * @principal)/(1-(1+@rate)**@number)
   end
 
   def pmt
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:id", { :controller => "calculations", :action => "sqrt" })
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+  get("/payment/:rate/:number/:principal", { :controller => "calculations", :action => "loan_payment" })
+
 end
